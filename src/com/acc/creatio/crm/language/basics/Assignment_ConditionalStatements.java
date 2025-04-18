ca6e84e868c7388410ec1faffda5a760b0e127bc
meghana <Meghana.durgabhavani@gmail.com>
2025-04-18 20:56:28 +0530

    Assignments

--- src/com/acc/creatio/crm/language/basics/Assignment_ConditionalStatements.java
+++ src/com/acc/creatio/crm/language/basics/Assignment_ConditionalStatements.java
@@ -7,31 +7,30 @@ public class Assignment_ConditionalStatements {
 		String CustName = "John Doe";
 		int CredScore = 720;
 		float income = 55000.0f;
-		String JohnDoe = "isEmployed"; 
 		int debtToIncomeRatio = 35;
+		boolean isEmployed = true;
 		
 		if(CredScore > 750)
 		{
 			System.out.println("Approve the loan directly");
 		}
-			else if(CredScore == 750 || CredScore >= 650)
+			else if( CredScore >= 650)
 			{
 				System.out.println("Yes " +CredScore+ " is CredScore Now ,Check the additional requirement");
 				if(income >= 50000.0)
 				{
-					System.out.println("Yes Incone is " +income+ " Now Check JohnDoe is employed or not");
+					System.out.println("Yes Income is " +income+ " Now Check JohnDoe is employed or not");
 				}
 				else if (income < 50000.0)
 				{
 					System.out.println("Your loan is denied");
 				}
-				 if(JohnDoe == "isEmployed" )
+				 if(isEmployed)
 				 {
-					 System.out.println("Yes John Doe " +JohnDoe+ " Now Check the debtToIncomeRatio");
+					 System.out.println("Now Check the debtToIncomeRatio");
 				 }
-				 else if(JohnDoe != "isEmployed")
-				 {
-				  System.out.println("Your not eligible for loan");
+				 else {
+					 System.out.println("Loan denied");
 				 }
 				 if(debtToIncomeRatio < 40)
 				 {
